12acbf6c523bb2634b21a65f1c2c702775dd5096
Tim Schindler <xh3b4sd@gmail.com>
2016-12-15 01:01:33 +0100

    fixed collection interface

--- collection/error.go
+++ collection/error.go
@@ -1,10 +1,30 @@
 package collection
 
-import "github.com/the-anna-project/storage/service/redis"
+import (
+	"fmt"
 
-// IsNotFound combines IsNotFound error matchers of all storage
-// implementations. IsNotFound should thus be used for error handling wherever
-// spec.Storage is dealt with.
-func IsNotFound(err error) bool {
-	return redis.IsNotFound(err)
+	"github.com/juju/errgo"
+)
+
+var (
+	maskAny = errgo.MaskFunc(errgo.Any)
+)
+
+func maskAnyf(err error, f string, v ...interface{}) error {
+	if err == nil {
+		return nil
+	}
+
+	f = fmt.Sprintf("%s: %s", err.Error(), f)
+	newErr := errgo.WithCausef(nil, errgo.Cause(err), f, v...)
+	newErr.(*errgo.Err).SetLocation(1)
+
+	return newErr
+}
+
+var invalidConfigError = errgo.New("invalid config")
+
+// IsInvalidConfig asserts invalidConfigError.
+func IsInvalidConfig(err error) bool {
+	return errgo.Cause(err) == invalidConfigError
 }
