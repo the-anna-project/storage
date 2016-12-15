614b3fd062ffac08fb955285bbe082acaf36b441
Tim Schindler <xh3b4sd@gmail.com>
2016-12-15 01:23:06 +0100

    separated common error (#7)

---
+++ error/error.go
@@ -0,0 +1,12 @@
+package error
+
+import (
+	"github.com/the-anna-project/storage/redis"
+)
+
+// IsNotFound combines IsNotFound error matchers of all storage
+// implementations. IsNotFound should thus be used for error handling wherever
+// spec.Storage is dealt with.
+func IsNotFound(err error) bool {
+	return redis.IsNotFound(err)
+}
